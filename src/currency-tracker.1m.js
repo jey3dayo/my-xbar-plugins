fee0025f5550c724542c1593dcd05510afd41ac2
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2020-10-30 18:32:01 +0900

    見た目整えた

--- src/currency-tracker.1m.js
+++ src/currency-tracker.1m.js
@@ -1,8 +1,42 @@
 #!/usr/bin/env /Users/t00114/.nodebrew/current/bin/node
 
 const fetch = require('node-fetch');
+const { exec } = require('child_process');
 
-const currencyType = ['USDJPY', 'GBPUSD'];
+const types = { usdjpy: 'USDJPY', gbpusd: 'GBPUSD' };
+const currencyType = [types.usdjpy, types.gbpusd];
+
+const alert = () => {
+  exec(`say ping pon`);
+  // const times = 3;
+  // [...Array(3)].forEach((_, repeat) => {
+  //   exec(`say ${repeat} ping pon`);
+  // });
+};
+
+const coloring = (v, color = 'red') => {
+  return `${v} | color=${color}`;
+};
+
+const formated = ({ pair, bid }) => {
+  const ret = `${pair}: ${bid}`;
+
+  if (pair === 'USDJPY') {
+    if (bid < 104.2) {
+      alert();
+      return coloring(ret, 'red');
+    }
+  }
+
+  if (pair === 'GBPUSD') {
+    if (bid < 1.2924) {
+      alert();
+      return coloring(ret, 'red');
+    }
+  }
+
+  return ret;
+};
 
 const main = async () => {
   const res = await fetch('https://www.gaitameonline.com/rateaj/getrate');
@@ -11,8 +45,14 @@ const main = async () => {
   const pairs = result.quotes
     .filter(v => currencyType.includes(v.currencyPairCode))
     .map(v => ({ bid: v.bid, pair: v.currencyPairCode }));
+
+  const head = pairs.shift();
+  console.log(formated(head));
+  console.log('---');
+
   pairs.forEach(v => {
-    console.log(`${v.pair}: ${v.bid}`);
+    const output = formated(v);
+    console.log(output);
   });
 };
 
